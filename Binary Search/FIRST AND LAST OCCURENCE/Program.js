5aecacd2356074887a02fe4c7a94c15c9ad6120e
ajaykumaraw <378ajaykumar@gmail.com>
2023-06-08 18:26:33 +0530

    problem 1

---
+++ Binary Search/FIRST AND LAST OCCURENCE/Program.js
@@ -0,0 +1,10 @@
+/* Given a sorted integer array, find the  index of a given
+   number's first or last occurance. if the element is not 
+   present in the array, report that as well.
+   
+   provide Input : nums:[2,5,5,5,6,6,8,9,9,9]
+   target : 5
+*/
+
+
+
