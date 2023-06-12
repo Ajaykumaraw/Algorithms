cfd75fa299796b2954c9c246f1a1a02000695517
ajaykumaraw <378ajaykumar@gmail.com>
2023-06-12 23:36:03 +0530

    BS

--- Binary Search/BinarySearch.js
+++ Binary Search/BinarySearch.js
@@ -4,7 +4,10 @@ function BinarySearch(arr,x){
     let mid;
     while(r>=l){
         mid = l + Math.floor((r-l)/2);
+        //if mid is equal to target then return mid 
         if(arr[mid]==x) return mid;
+        /*  if value at mid is grater then target 
+         then shift right to mid -1  else left to mid+1  */
         if(arr[mid]>x) r = mid -1;
         else
             l = mid+1
@@ -13,5 +16,5 @@ function BinarySearch(arr,x){
 }
 
 let arr = [2,4,6,8,12,18,22,49,50];
-const resutlt = BinarySearch(arr,24);
+const resutlt = BinarySearch(arr,2);
 console.log(resutlt);
