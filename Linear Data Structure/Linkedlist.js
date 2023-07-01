0e8dd58d0203a1d1b0edfa121ce7fc038c708719
ajaykumaraw <378ajaykumar@gmail.com>
2023-07-01 17:10:12 +0530

    ls

--- Linear Data Structure/Linkedlist.js
+++ Linear Data Structure/Linkedlist.js
@@ -2,16 +2,43 @@
     structure and will perform basic operations like 
     insertion deletion seaching and traversal.
 */
-
 class LinkedList{
-    constructor(head=null){
-        this.head = head
+    constructor(head){
+        this.head = null
+    }
+
+    print(){
+        let node = this.head
+        if(!node) console.log("empty list")
+        while(node.next){
+            node = node.next
+            console.log(node.data)
+        }
+    }
+
+    appendList(newnode){
+        let node = this.head
+        if(node ==null) {
+            console.log("node null")
+            this.head = newnode
+            return
+        }
+        while(node.next){
+            node = node.next
+        }
+        node.next = newnode
     }
 }
 
 class Node{
     constructor(data){
         this.data = data
-        this.next = null;
+        this.next = null
     }
 }
+
+
+let linkedlist = new LinkedList();
+let node = new Node(5);
+linkedlist.appendList(node)
+linkedlist.print();
